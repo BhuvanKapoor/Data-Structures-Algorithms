9c11206ec0c2ff2191c807d0bc03ac8c000721c6
Bhuvan Kapoor <105715714+BhuvanKapoor@users.noreply.github.com>
2022-10-07 17:58:33 +0530

    Update Row-Major-Matrix.cpp

--- Matrices/Row-Major-Matrix.cpp
+++ Matrices/Row-Major-Matrix.cpp
@@ -23,10 +23,8 @@ int main () {
     int matrix [row][col], k = 0;
 
     for (int i = 0; i < row; i++)
-        for (int j = 0; j < col; j++) {
-            matrix[i][j] = arr[k];
-            k++;
-        }
+        for (int j = 0; j < col; j++)
+            matrix[i][j] = arr[k++];
 
     cout << "\nRow Major Matrix " << endl;
     for (int i = 0; i < row; i++) {
